62aee423810dfa78a285619f55962b0cc8367094
Ryan Southall <rgsouthall@gmail.com>
2019-11-04 21:22:45 +0000

    Envi nodes 2

---
+++ vi_dicts.py
@@ -0,0 +1,7 @@
+unit_dict = {'Lux': 'illu', 'DF (%)': 'df', u'W/m\u00b2 (v)': 'virrad', u'W/m\u00b2 (f)': 'firrad', 'Sky View': 'sv', 'sDA (%)': 'sda', 'ASE (hrs)': 'ase',
+             'Mlxh': 'mlxh','kWh (f)': 'fi', u'kWh/m\u00b2 (f)': 'fim2', 'kWh (v)': 'vi', u'kWh/m\u00b2 (v)': 'vim2', 'DA (%)': 'da', 'UDI-f (%)': 'udil', 'UDI-s (%)': 'udis', 
+             'UDI-a (%)': 'udia', 'UDI-e (%)': 'udie', 'kWh': 'kwh', 'kWh/m2': 'kwhm2'}
+
+colours = [('rainbow', 'Rainbow', 'Rainbow colour scale'), ('gray', 'Grey', 'Grey colour scale'), ('hot', 'Hot', 'Hot colour scale'),
+           ('CMRmap', 'CMR', 'CMR colour scale'), ('jet', 'Jet', 'Jet colour scale'), ('plasma', 'Plasma', 'Plasma colour scale'), 
+           ('hsv', 'HSV', 'HSV colour scale'), ('viridis', 'Viridis', 'Viridis colour scale')]
