89994add25be6ecce84fbef51075daa5c75321ed
rgsouthall <rgsouthall@gmail.com>
2023-09-26 18:32:24 +0100

    Try another OS X fix

--- vi_chart.py
+++ vi_chart.py
@@ -324,7 +324,8 @@ def hmchart_disp(chart_op, plt, dnode, col):
     plt.xticks(size=14)
     plt.yticks(size=14)
     fig.tight_layout()
-    #plt.ion()
+    if sys.platfrom == 'darwin':
+        plt.ion()
     plt.show()
 
 
